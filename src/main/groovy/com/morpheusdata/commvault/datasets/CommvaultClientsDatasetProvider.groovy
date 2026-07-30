2c1bae522bde705c382badbf932ac32913243490
Jordan Sutton <jordan.sutton@hpe.com>
2026-07-30 11:24:38 -0600

    Fixed No client setup in commvault logic

--- src/main/groovy/com/morpheusdata/commvault/datasets/CommvaultClientsDatasetProvider.groovy
+++ src/main/groovy/com/morpheusdata/commvault/datasets/CommvaultClientsDatasetProvider.groovy
@@ -131,11 +131,10 @@ class CommvaultClientsDatasetProvider extends AbstractDatasetProvider<ReferenceD
                     def clientInstanceTypeCode = clientInstanceType ? CommvaultReferenceUtility.getvsInstanceType(clientInstanceType?.toString()) : null
                     if (!cloud || cloud?.cloudType?.provisionTypes?.find { it.code == clientInstanceTypeCode }) {
                         clients << [name: client.name, id: client.id, value: client.id]
-                    } else {
-                        clients << [name: "Clients not available for this cloud type", id: '']
                     }
                 }
-            } else {
+            }
+            if(clients.isEmpty()) {
                 clients << [name: "No clients setup in Commvault", id: '']
             }
         } else {
